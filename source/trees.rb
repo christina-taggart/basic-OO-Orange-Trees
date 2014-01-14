40bd04325c166ec5d5c2c9005f1045c911f30d26
Darcey Lachtman & <pair+dslachtman.hzla00@devbootcamp.com>
2014-01-13 16:26:40 -0800

    release 2

--- source/trees.rb
+++ source/trees.rb
@@ -43,6 +43,46 @@ class OrangeTree < Tree
   end
 end
 
+class AppleTree < Tree
+  def initialize
+    super(4, 35, Apple, 23, 4, 5, "Apple Tree")
+  end
+end
+
+class PearTree < Tree
+  def initialize
+    super(2, 23, Pear, 19, 9, 4, "Pear Tree" )
+  end
+end
+
+class TreeGrove
+  def initialize(orange = 0, apple = 0, pear = 0)
+    @grove = []
+    orange.times{@grove << OrangeTree.new}
+    apple.times{@grove << AppleTree.new}
+    pear.times{@grove << PearTree.new}
+  end
+
+  def age!
+    @grove.each do |tree|
+      tree.age!
+    end
+  end
+
+  def trees
+    @grove.map{|tree| tree.name}
+  end
+
+  def mature_trees
+    @grove.find_all{|tree| tree.age > tree.maturity_age}.map {|tree| tree.name}
+  end
+
+  def dead_trees
+    @grove.find_all{|tree| tree.dead}.map {|tree| tree.name}
+  end
+end
+
+
 class Fruit
   attr_reader :name, :avg_size, :variance, :diameter
   def initialize(name, avg_size, variance)
@@ -56,6 +96,18 @@ end
 class Orange < Fruit
   def initialize
     super("Orange", 4, 2)
+  end
+end
+
+class Apple < Fruit
+  def initialize
+    super("Apple", 5, 3)
+  end
+end
+
+class Pear < Fruit
+  def initialize
+    super("Pear", 3, 2)
   end
 end
 
@@ -77,20 +129,32 @@ class Person
   end
 end
 
-orange_tree = OrangeTree.new
-person = Person.new
+# orange_tree = OrangeTree.new
+# person = Person.new
 
-while orange_tree.dead != true
-  orange_tree.age!
-  person.pick(orange_tree) if orange_tree.has_fruit
-end
+# while orange_tree.dead != true
+#   orange_tree.age!
+#   person.pick(orange_tree) if orange_tree.has_fruit
+# end
 
-puts "Year #{orange_tree.age} Report:"
-puts "Tree height is #{orange_tree.height}"
-puts "Harvest is #{person.basket.length}"
+# puts "Year #{orange_tree.age} Report:"
+# puts "Tree height is #{orange_tree.height}"
+# puts "Harvest is #{person.basket.length}"
 
-person.calc_diameter
-puts "Average Diameter is #{person.avg}"
+# person.calc_diameter
+# puts "Average Diameter is #{person.avg}"
+
+grove = TreeGrove.new(5,5,5)
+
+30.times {grove.age!}
+
+
+puts "All Trees:"
+p grove.trees
+puts "Dead Trees"
+p grove.dead_trees
+puts "Mature Trees"
+p grove.mature_trees
 
 
 
