36f48250ca60b0ba5c028474f4492f01a1bc40c5
David Goodman <null>
2014-01-13 16:50:49 -0800

    got orange tree working will continue to play with it

--- source/orange_tree.rb
+++ source/orange_tree.rb
@@ -3,14 +3,27 @@ class NoOrangesError < StandardError
 end
 
 class OrangeTree
+  attr_accessor :height, :age
   # Ages the tree one year
+
+  def initialize(age = 0, height = 3)
+    @age = age
+    @height = height
+  end
+
   def age!
+    @age +=1
+    @age > 10 ? @height : @height += 1
   end
 
   # Returns +true+ if there are any oranges on the tree, +false+ otherwise
   def any_oranges?
+    @age > 5 ? true : false
   end
 
+  def dead?
+    @age > 24 ? true : false
+  end
   # Returns an Orange if there are any
   # Raises a NoOrangesError otherwise
   def pick_an_orange!
@@ -21,7 +34,9 @@ class OrangeTree
 end
 
 class Orange
+  attr_reader :diameter
   # Initializes a new Orange with diameter +diameter+
   def initialize(diameter)
+    @diameter = diameter
   end
 end
